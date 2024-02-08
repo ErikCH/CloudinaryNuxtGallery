44985917a451c6c64ab9ef82f583f473a2bfed3d
Erik Hanchett <icystorm@gmail.com>
2024-02-07 20:56:14 -0800

    added command

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/cloudinary", "@nuxtjs/tailwindcss"]
+  modules: ["@nuxtjs/cloudinary", "@nuxtjs/tailwindcss"],
+  cloudinary: {
+    cloudName: "dzffvum1n"
+  }
 });
