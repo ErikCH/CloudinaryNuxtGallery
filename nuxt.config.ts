12815da4596044dc29578a194d5b7c9cb317610e
Erik Hanchett <icystorm@gmail.com>
2024-02-07 22:01:49 -0800

    Added config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,9 @@ export default defineNuxtConfig({
   modules: ["@nuxtjs/cloudinary", "@nuxtjs/tailwindcss"],
   cloudinary: {
     cloudName: "dzffvum1n"
+  },
+  runtimeConfig: {
+    cloudSecret: process.env.NUXT_CLOUD_SECRET_KEY,
+    cloudKey: process.env.NUXT_CLOUD_API_KEY
   }
 });
--- server/api/cloud.ts
+++ server/api/cloud.ts
@@ -1,5 +1,7 @@
 import cloudinary from "cloudinary";
 export default defineEventHandler(async (event) => {
+  const environmental = useRuntimeConfig(event);
+
   interface Results {
     resources: [
       {
@@ -8,7 +10,12 @@ export default defineEventHandler(async (event) => {
     ];
   }
 
-  // test2
+  cloudinary.v2.config({
+    cloud_name: environmental.public.cloudinary.cloudName,
+    api_key: environmental.cloudKey,
+    api_secret: environmental.cloudSecret
+  });
+
   const data: Results = await cloudinary.v2.search
     .expression("resource_type:image")
     .sort_by("public_id", "desc")
